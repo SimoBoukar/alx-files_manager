9ca6ffc5b9031981745dfcd1770ac511fb7e28f0
Mohammed <6bouass9@gmail.com>
2024-10-02 18:56:21 +0100

    push task 5,6,7,8 and 9

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const router = Router();
 router.get('/status', AppController.getStatus);
@@ -10,5 +11,11 @@ router.post('/users', UsersController.postNew);
 router.get('/users/me', UsersController.getMe);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files/:id/data', FilesController.getFile);
+router.get('/files', FilesController.getIndex);
+router.post('/files', FilesController.postUpload);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
 export default router;
