5f23819d344cbb4f1c831cc9cfa6d81814343416
Mohammed <6bouass9@gmail.com>
2024-10-02 13:11:53 +0100

    push task 3

---
+++ controllers/AppController.js
@@ -0,0 +1,20 @@
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+class AppController {
+  static async getStatus(req, res) {
+    const redisIsAlive = redisClient.isAlive();
+    const dbIsAlive = dbClient.isAlive();
+    const status = { redis: redisIsAlive, db: dbIsAlive };
+    await res.status(200).json(status);
+  }
+
+  static async getStats(req, res) {
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
+    const stats = { users: usersCount, files: filesCount };
+    res.status(200).json(stats);
+  }
+}
+
+export default AppController;
